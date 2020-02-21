93ac7d216b4d2f820d14d48a0d30dc9bbd4cf357
haiyongsong1921 <sunhy339@126.com>
2020-02-21 15:45:56 +0800

    add gyo sensor feature

--- drive_slope/main.py
+++ drive_slope/main.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+from GyroCar import GyroCar
+from ev3dev2.sound import Sound
+from ev3dev2.motor import OUTPUT_A, OUTPUT_D
+import sys
+
+
+sound = Sound()
+sound.beep()
+
+gyroCar = GyroCar(OUTPUT_A, OUTPUT_D, 'GYRO-ANG')
+gyroCar.reset_angle()
+print("%d" % gyroCar.angle)
+reach_slope = False
+
+while True:
+    gyroCar.run()
+    on_slope = gyroCar.is_on_slope()
+    if(on_slope):
+        reach_slope = True
+
+    reach_to_top = gyroCar.is_on_flat()
+    if(reach_to_top and reach_slope):
+        gyroCar.run_rotations(1)
+        gyroCar.stop()
+        break
